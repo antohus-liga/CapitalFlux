e7bfcf3bea974e919554734442b0809314ded6f8
antohus-liga <antonioslotylo0904@gmail.com>
2026-06-05 12:42:45 +0100

    add routes and dynamic titles

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from "@/features/users/pages/LoginView.vue"
+import RegisterView from "@/features/users/pages/RegisterView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/login",
+      component: LoginView,
+      meta: { title: "Sign in" }
+    },
+    {
+      path: "/signup",
+      component: RegisterView,
+      meta: { title: "Sign up" }
+    }
+  ],
+})
+
+router.beforeEach((to) => {
+  const { title } = to.meta;
+  document.title = title
 })
 
 export default router
