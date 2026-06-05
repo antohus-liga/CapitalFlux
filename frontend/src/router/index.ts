a1cf4262199407355f9cfa0a3e5bc0372c88a637
antohus-liga <antonioslotylo0904@gmail.com>
2026-06-05 12:52:50 +0100

    fix type error

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from "@/features/users/pages/LoginView.vue"
 import RegisterView from "@/features/users/pages/RegisterView.vue"
 
+declare module 'vue-router' {
+  interface RouteMeta {
+    title: string
+  }
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,7 +25,7 @@ const router = createRouter({
 })
 
 router.beforeEach((to) => {
-  const { title } = to.meta;
+  const title = to.meta.title
   document.title = title
 })
 
